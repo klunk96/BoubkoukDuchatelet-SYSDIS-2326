d53afb0475c1db026b1788b1c3dd7a1d89c37458
yassine <yassine.boubkouk@student.hepl.be>
2021-09-01 18:33:30 +0200

    push shopping cart

--- src/main/java/com/BoubkoukDuchatelet/Market/controller/AdminController.java
+++ src/main/java/com/BoubkoukDuchatelet/Market/controller/AdminController.java
@@ -80,7 +80,7 @@ public class AdminController {
             page = Integer.parseInt(pageStr);
         } catch (Exception e) {
         }
-        final int MAX_RESULT = 5;
+        final int MAX_RESULT = 6;
         final int MAX_NAVIGATION_PAGE = 10;
 
         PaginationResult<OrderInfo> paginationResult //
